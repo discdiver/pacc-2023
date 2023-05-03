801c9de0cf82a81f9a6e4fe2dbe6304483630f0b
Jeff Hale <jeffmshale@gmail.com>
2023-05-03 17:03:47 -0400

    add s3-bucket creation code

--- 202/upload_to_s3.py
+++ 202/upload_to_s3.py
@@ -7,7 +7,7 @@ from prefect_aws.s3 import S3Bucket
 def upload_to_s3(color: str, year: int, month: int) -> None:
     """The main flow function to upload taxi data"""
     path = Path(f"data/{color}/{year}/{color}_tripdata_{year}-{month:02}.parquet")
-    s3_block = S3Bucket.load("prefect-s3-bucket")
+    s3_block = S3Bucket.load("s3-bucket-block")
     s3_block.upload_from_path(from_path=path, to_path=path)
 
 
