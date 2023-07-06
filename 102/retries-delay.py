2304e753b42810ac3dd26e7dcb6957254039fc64
Jeff Hale <jeffmshale@gmail.com>
2023-07-06 10:15:02 -0400

    update retries api link

--- 102/retries-delay.py
+++ 102/retries-delay.py
@@ -4,7 +4,7 @@ from prefect import flow, task
 
 @task(retries=4, retry_delay_seconds=0.1)
 def fetch_cat_fact():
-    cat_fact = httpx.get("https://f3-vyx5c2hfpq-ue.a.run.app/")
+    cat_fact = httpx.get("https://httpstat.us/Random/200,500")
     if cat_fact.status_code >= 400:
         raise Exception()
     print(cat_fact.text)
--- 102/retry-flow.py
+++ 102/retry-flow.py
@@ -4,7 +4,7 @@ from prefect import flow
 
 @flow(retries=4)
 def fetch():
-    cat_fact = httpx.get("https://f3-vyx5c2hfpq-ue.a.run.app/")
+    cat_fact = httpx.get("https://httpstat.us/Random/200,500")
     if cat_fact.status_code >= 400:
         raise Exception()
     print(cat_fact.text)
