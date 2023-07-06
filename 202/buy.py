f37f093f59a8724475bd762a80d144defce80a61
Jeff Hale <jeffmshale@gmail.com>
2023-07-06 08:38:27 -0400

    updates for july 6

--- 202/buy.py
+++
@@ -1,7 +0,0 @@
-from prefect import flow
-
-
-@flow(log_prints=True)
-def buy():
-    """buy securities"""
-    print("Bought securities")
--- 202/sell.py
+++
@@ -1,7 +0,0 @@
-from prefect import flow
-
-
-@flow(log_prints=True)
-def sell():
-    """Sell securities"""
-    print("Sold securities!")
